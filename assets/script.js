5b39b02d52597d4f2f73dfaacc6ad0853bb9fa7c
Timothy <timsu100@gmail.com>
2023-07-12 21:26:16 -0700

    add convert to location to current weather

--- assets/script.js
+++ assets/script.js
@@ -24,22 +24,49 @@ var citySubmit = function (event) {
 var cityBtnClick = function (event) {
 
 };
-
+// gets longitude and latitude of desired city
 var getCity = function (city) {
   console.log(city);
   var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
 
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      response.json().then(function(data){
+      response.json().then(function (data) {
         console.log(data);
-
+        getCurrentWeather(data)
       });
-    }else{
-      alert('Error:' + response.statusText);
+    } else {
+      alert('Error: ' + response.statusText);
     }
   });
 };
+
+// convert longitude and latitude to local weather
+
+var getCurrentWeather = function (city) {
+  if (city.length === 0) {
+    currentWeather.textContent = "City not found";
+    return;
+  }
+
+  var lon = city[0].lon;
+  var lat = city[0].lat;
+  var cityName = city[0].name;
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+  console.log(cityName + ': ' + lon + ', ' + lat)
+
+  fetch(apiUrl).then(function(response){
+    if (response.ok){
+      response.json().then(function(data){
+        console.log(data);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
+};
+
+
 
 cityFormEl.addEventListener('submit', citySubmit);
 
