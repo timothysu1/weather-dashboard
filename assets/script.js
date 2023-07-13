bff4df834dec89298a2110d9658671eebc079c08
Timothy <timsu100@gmail.com>
2023-07-12 20:39:20 -0700

    add city submit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
-//api key d17b385827453e64acea3cc46849537a
+var apiKey = "d17b385827453e64acea3cc46849537a";
+var cityContainerEl = document.querySelector("#city-container");
+var fiveDayContainerEl = document.querySelector(".five-day-container");
+var cityFormEl = document.querySelector("#city-form");
+var currentWeather = document.querySelector(".current-weather");
+var cityInput = document.querySelector("#city")
+
+var citySubmit = function (event) {
+  event.preventDefault();
+
+  var city = cityInput.value.trim();
+  if (city) {
+    getCity(city);
+
+    currentWeather.textContent = "";
+    fiveDayContainerEl.textContent = "";
+    cityInput.value = "";
+  } else {
+    alert("Please enter a city")
+  }
+
+};
+
+var cityBtnClick = function (event) {
+
+};
+
+var getCity = function (city) {
+  console.log(city);
+};
+
+cityFormEl.addEventListener('submit', citySubmit);
+
+
+
